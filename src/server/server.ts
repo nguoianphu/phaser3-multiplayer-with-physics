453201f3b035a5d0902d79ff87fc67a452238877
nguoianphu <nguoianphu@gmail.com>
2019-05-07 04:43:29 +0000

    port 8080

--- src/server/server.ts
+++ src/server/server.ts
@@ -15,7 +15,7 @@ import Routes from './routes/routes'
 import IoStats from './socket/ioStats'
 import IoGame from './socket/ioGame'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 // create 2 socket.io namespaces
 const ioNspGame = io.of('/G' /* short for stats */)
